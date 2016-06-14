f97ba6537cc0e2295b1965541c7b263f1314ce7b
jianghailong <395729207@qq.com>
2016-06-14 18:26:04 +0800

    update 3

--- 3_longest_substring_without_repeating_characters.py
+++ 3_longest_substring_without_repeating_characters.py
@@ -2,39 +2,13 @@ class Solution(object):
 	def lengthOfLongestSubstring(self, s):
 		max_len = 0
 		s_index_dict = {}
-		s_len_dict = {}
-		
+		index = 0
+
 		for i in range(len(s)):
-			key = s[i]
-			#match the same char
-			if not key in s_index_dict:
-				s_len_dict[key] = 0
-				s_index_dict[key] = i
-			else:
-				match_str_len = i - s_index_dict[key]
-				if match_str_len > max_len:
-					max_len = match_str_len
-				
-				#delete useless information after the matching
-				will_del = []
-				for s_tmp in s_len_dict:
-					s_len = i - s_index_dict[s_tmp]
-					if s_len > max_len:
-						max_len = s_len
-					if s_index_dict[s_tmp] < s_index_dict[key]:
-						will_del.append(s_tmp)
-				for s_tmp in will_del:
-					del s_len_dict[s_tmp]
-					del s_index_dict[s_tmp]
-
-				s_len_dict[key] = match_str_len
-				s_index_dict[key] = i
-
-		#end of the match
-		for key in s_len_dict:
-			s_len = len(s) - s_index_dict[key]
-			if s_len > max_len:
-				max_len = s_len
+			if s[i] in s_index_dict:
+				index = max(index, s_index_dict[s[i]] + 1)
+			max_len = max(max_len, i - index + 1)
+			s_index_dict[s[i]] = i
 
 		return max_len
 
@@ -53,4 +27,6 @@ print test_str + " max_len:%d\n" % (test.lengthOfLongestSubstring(test_str));
 test_str = "aab"
 print test_str + " max_len:%d\n" % (test.lengthOfLongestSubstring(test_str));
 test_str = "dvdf"
+print test_str + " max_len:%d\n" % (test.lengthOfLongestSubstring(test_str));
+test_str = "c"
 print test_str + " max_len:%d\n" % (test.lengthOfLongestSubstring(test_str));
