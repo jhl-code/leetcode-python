b8aa1e70a090a491b90ed10ba54d61f280d56601
jianghailong <395729207@qq.com>
2016-06-21 15:44:43 +0800

    5

--- 5_longest_palindromic_substring.py
+++ 5_longest_palindromic_substring.py
@@ -16,21 +16,27 @@ class Solution(object):
                 if len(substring) < 1:
                     substring = s[l[0]]
                 continue
+            max_len = 0
             for i in range(len(l)):
+                if max_len >= len(s) - l[i]:
+                    break
                 if i >= len(l) - 1:
                     break
-                if self.checkPalindrome(s, l[i], l[i+1]):
-                    if len(substring) < l[i+1]-l[i]+1:
-                        substring = s[l[i]:l[i+1]+1]
+                for j in range(i+1, len(l))[::-1]:
+                    if max_len >= l[j]-l[i]+1:
+                        break
+                    if self.checkPalindrome(s, l[i], l[j]):
+                        if len(substring) < l[j]-l[i]+1:
+                            substring = s[l[i]:l[j]+1]
+                            max_len = l[j]-l[i]+1
+                        break
         return substring
 
     def checkPalindrome(self, s, i1, i2):
-        while i1 <= i2:
-            if s[i1] != s[i2]:
-                return False
-            i1 += 1
-            i2 -= 1
+        if s[i1:i2+1][::-1] == s[i1:i2+1]:
             return True
+        else:
+            return False
 
 test = Solution()
 test_str = "123456787654567"
@@ -39,3 +45,14 @@ test_str = "a"
 print "%s:%s" % (test_str, (test.longestPalindrome(test_str)))
 test_str = "cccaccc"
 print "%s:%s" % (test_str, (test.longestPalindrome(test_str)))
+test_str = "aaabaaaa"
+print "%s:%s" % (test_str, (test.longestPalindrome(test_str)))
+test_str = "aaaabaaa"
+print "%s:%s" % (test_str, (test.longestPalindrome(test_str)))
+test_str = "abababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababa"
+print "%s:%s" % (test_str, (test.longestPalindrome(test_str)))
+test_str = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabcaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+print "%s:%s" % (test_str, (test.longestPalindrome(test_str)))
+test_str = "tembwxtvddsttolegryohdlhxhycymqybzfzcbkzdwcekzfapmpfyeivfoeeqoqdhylziqpnyzuzeeutpqpalbddlliuehvkhqevgjdkskvphidcjmpcmetzwqkzcnxjcjywhfzplntbkuddmbcovearburjqyirbladcrhfkfdfgsmyhdsfmmxmslwkymkgaguilxghmfgaldcogtfnbqakctqtqakupwrxkmbjpmzqngwldmaugzizgwmediyzxevspxdwruyzrmnhchtxlgtb"
+print "%s:%s" % (test_str, (test.longestPalindrome(test_str)))
+
